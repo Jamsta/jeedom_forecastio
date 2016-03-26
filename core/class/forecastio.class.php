da802e768195ac42eeefd9fe46074df52ff3c07d
lunarok <lunarok@gmail.com>
2016-03-27 00:04:32 +0100

    icone mobile

--- core/class/forecastio.class.php
+++ core/class/forecastio.class.php
@@ -1709,14 +1709,14 @@ class forecastio extends eqLogic {
       $press .= '"' . $value['time'] . '":"' . $value['pressure'] . '"';
     }
     $status = '{"summary":,"icon":,"temperature":,"apparentTemperature":,"humidity":,"precipProbability":,"windSpeed":,"windBearing":,"cloudCover":,"pressure":}';
-    $hour =
-    $day0 =
-    $day1 =
-    $day2 =
-    $day3 =
-    $day4 =
-    $day5 =
-    $day6 =
+    $hour =;
+    $day0 =;
+    $day1 =;
+    $day2 =;
+    $day3 =;
+    $day4 =;
+    $day5 =;
+    $day6 =;
   }
 
   public function getGeoloc($_infos = '') {
